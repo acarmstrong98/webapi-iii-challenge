1d2409b16edbaf1a5b47afbcfbfadea1f63e8dc9
Andrew <acarmstrong98@gmail.com>
2019-03-21 21:46:04 -0400

    added GET with ID and POST

---
+++ data/routers/user-router.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const UserData = require('../helpers/userDb.js');
+
+const router = express.Router();
+
+
+
+// GET
+
+router.get('/', async (req, res) => {
+  try {
+      const users = await UserData.get(req.query);
+      res.status(200).json(users);
+
+  } catch (error) {
+
+     // log error to database
+      console.log(error);
+      res.status(500).json({ error: "The users information could not be retrieved." });
+  }
+});
+
+// GET by id
+
+router.get('/:id', async (req, res) => {
+  try {
+
+        const user = await UserData.getById(req.params.id);
+
+      if (user) {
+          res.status(200).json(user);
+      } 
+      
+      else {
+          res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+
+  } catch (error) {
+
+      // log error to database
+      console.log(error);
+      res.status(500).json({ error: "The user information could not be retrieved." });
+  } 
+});
+
+//POST
+
+router.post('/', async (req, res) => {
+
+    if (!req.body.name) {
+       res.status(400).json({ errorMessage: "Please provide a name for the user." });
+   } else {    
+       try {
+           const user = await UserData.insert(req.body);
+           res.status(201).json(user);
+       } catch (err) {
+        res.status(500).json({error: "There was an error while saving the post to the database."});
+      }
+   }
+  
+  
+  });
+  
+
+module.exports = router; 
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+
+const userRouter = require('./data/routers/user-router.js')
+const postRouter = require('./data/routers/post-router.js')
+
+const server = express();
+const parser = express.json();
+const logMiddleware = logger('dev');
+const securityMiddleware = helmet();
+
+server.use(parser, logMiddleware, securityMiddleware);
+server.use('/api/users', userRouter, userRouter);
+
+server.get('/', async (req, res) => {
+  res.send(`
+    <h2>TESTING...</h2>`);
+});
+
+module.exports = server; 
